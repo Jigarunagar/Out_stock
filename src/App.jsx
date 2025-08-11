724878fba65820c2ac4d9b62eebec069779ab9be
Jigar Unagar <officialjigarunagar@gmail.com>
2025-08-11 11:56:15 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
-  
   return (
     <>
-    
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
